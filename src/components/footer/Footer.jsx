fbae6fee01e02692531e736561b8397c33f27297
João Falcão <jgabfalcao@gmail.com>
2023-07-13 10:59:57 -0300

    feat: Inserindo dados

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,57 @@
 import React from "react";
-import './footer.css'
+import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
 
 const Footer = () => {
-  return(
-    <section>Footer</section>
-  )
-}
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        JOÃO
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Experience</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Portfolio</a>
+        </li>
+        <li>
+          <a href="#">Comments</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; JOÃO FALCÃO Developer. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
