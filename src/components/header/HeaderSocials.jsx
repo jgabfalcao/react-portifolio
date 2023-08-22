896deb2d84fb14a74bee45386aaf594c0484ae0e
João Falcão <jgabfalcao@gmail.com>
2023-08-22 18:13:17 -0300

    fix: Inserindo links redes sociais

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-import {BsLinkedin, BsInstagram} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
-
-const HeaderSocials = () =>{
-  return(
+const HeaderSocials = () => {
+  return (
     <div className="header__socials">
-      <a href="https://linkedin.com/" target="_blank" ><BsLinkedin/></a>
-      <a href="https://github.com/" target="_blank" ><FaGithub/></a>
-      <a href="https://instagram.com/" target="_blank" ><BsInstagram/></a>
+      <a
+        href="https://www.linkedin.com/in/jo%C3%A3o-falc%C3%A3o013/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/jgabfalcao" target="_blank" rel="noreferrer">
+        <FaGithub />
+      </a>
+      <a
+        href="https://www.instagram.com/joaaofalcao/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsInstagram />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
